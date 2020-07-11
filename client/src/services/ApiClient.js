e795763bf245822315be1c9f8e7cc9f6e4f3a51e
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2020-07-11 20:19:33 +0200

    chore: switched data to mockData

--- client/src/services/ApiClient.js
+++ client/src/services/ApiClient.js
@@ -1,23 +1,23 @@
 // Uncomment lines 3 and 9, while commenting out lines 7 to use mock data in place of the database.
 
-// import mongoMocks from './../__mocks__/mongoMocks.json'; // Import mock data.
-const BASE_URL = process.env.REACT_APP_API_URL;
+import mongoMocks from './../__mocks__/mongoMocks.json'; // Import mock data.
+// const BASE_URL = process.env.REACT_APP_API_URL;
 
 function getTechnologies() {
-  return fetchRequest('/tweetdata'); // Comment this out if you want to use mock data.
+  // return fetchRequest('/tweetdata'); // Comment this out if you want to use mock data.
 
-  // return Promise.resolve(mongoMocks); // Use this lines to access mock data for the client.
+  return Promise.resolve(mongoMocks); // Use this lines to access mock data for the client.
 }
 
-function fetchRequest(path, options) {
-  return fetch(BASE_URL + path, options)
-    .then((res) => (res.ok ? res : Promise.reject(res)))
-    .then((res) => (res.status !== 204 ? res.json() : res))
-    .catch(
-      (err) =>
-        console.log(`Error fetching [${options ? options.method : `GET`}]`, err) // eslint-disable-line
-    );
-}
+// function fetchRequest(path, options) {
+//   return fetch(BASE_URL + path, options)
+//     .then((res) => (res.ok ? res : Promise.reject(res)))
+//     .then((res) => (res.status !== 204 ? res.json() : res))
+//     .catch(
+//       (err) =>
+//         console.log(`Error fetching [${options ? options.method : `GET`}]`, err) // eslint-disable-line
+//     );
+// }
 
 export default {
   getTechnologies,
